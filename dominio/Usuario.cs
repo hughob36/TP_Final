9b7e237bca13477cfb655e5a4217d19629e159d7
Hugo Benitez <hugo.boca16@gmail.com>
2024-05-29 11:57:07 -0300

    cambios

---
+++ dominio/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dominio
+{
+    public class Usuario
+    {
+
+        public int Id { get; set; }
+
+        public string NombreUsuario { get; set; }
+
+        public string Contrasena { get; set; }
+
+        public string Email { get; set; }
+
+        public bool EsAdministrador { get; set; }
+
+        public  bool EsVip { get; set; } 
+
+
+    }
+}
